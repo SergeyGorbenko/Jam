2cc21174c8f9aadb245770e28849399b8b2bf715
dimon <dimonchikakus@gmail.com>
2018-04-08 00:26:05 +0300

    1

--- Cubic_UFO/main.py
+++ Cubic_UFO/main.py
@@ -1,4 +1,14 @@
+import math
+
 cases = int(input())
 i = 0
 while i < cases:
-    number = int(input())
+    number = float(input())
+    if number < 2 ** (1 / 2):
+        arc_cos = math.acos(1 / number)
+        number = math.cos(arc_cos) / 2
+    print('Case #' + str(i + 1) + ':')
+    print(str(number) + ' ' + str(number) + ' 0')
+    print('-' + str(number) + ' ' + str(number) + ' 0')
+    print('0 0 0.5')
+    i += 1
