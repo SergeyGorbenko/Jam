e80a1b4a6712501d656e2f1e43a048f87cb81964
Sergey Gorbenko <sergey13gorbenko@gmail.com>
2018-04-14 04:29:47 +0300

    1

---
+++ J1A/B/main.py
@@ -0,0 +1,6 @@
+T = int(input())
+                                                                                    # S*N+P
+for i in range(1, T + 1):
+    R, B, C = [int(x) for x in input().split()]
+    for j in range(C):
+        M, S, P = [int(x) for x in input().split()]
