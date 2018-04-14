78ed19260895bfcbf0c82acd9506b7032b467e19
Sergey Gorbenko <sergey13gorbenko@gmail.com>
2018-04-14 05:39:49 +0300

    1

--- J1A/B/main.py
+++ J1A/B/main.py
@@ -1,6 +1,24 @@
 T = int(input())
-                                                                                    # S*N+P
+# S*N+P
 for i in range(1, T + 1):
+    case = []
     R, B, C = [int(x) for x in input().split()]
+    n = B // R
+    if B % R != 0:
+        n += 1
     for j in range(C):
         M, S, P = [int(x) for x in input().split()]
+        case.append([M, S, P, M * S + P])
+    case.sort(key=lambda i:i[3])
+    count = 1
+    x = case[0]
+    for l in range(case.__len__()):
+        if case[l][3] == x[3]:
+            continue
+        count+=1
+        if count == B+1:
+            count = l
+            break
+    case = case[:count]
+    case.sort(key=lambda i: i[1])
+    print(case)
